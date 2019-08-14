546048d0a5df412b9ef82060819d82b6719614b7
brad <me@adrianbrad.com>
2019-08-15 00:00:49 +0300

    added websocket proxy

--- server.go
+++ server.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
-	"os"
-	"log"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		b,_ := ioutil.ReadAll(r.Body)
-		fmt.Fprintf(w,"Hello from %s\nreceived body: %s\n", os.Getenv("WHO"), b)
-	})))
+	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(helloHandler)))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "Hello from %s\nreceived body: %s\n", os.Getenv("WHO"), b)
 }
